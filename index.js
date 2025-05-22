17e51882e6a28ab420bf7f7c793a839b80a3ccc2
Rohit Mane <74919679+rohitmane9022@users.noreply.github.com>
2025-05-22 15:36:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 const connectDB = require('./db/connect');
 const jobRoutes = require('./routes/jobRoutes');
@@ -7,28 +8,24 @@ const authRoutes = require('./routes/authRoutes');
 // const recommendationRoutes = require('./routes/recommendations');
 
 const app = express();
+connectDB()
 
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
+//Routes Check
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 // app.use('/api/job-recommendations', recommendationRoutes);
 
-app.get("/", (req, res) => {
-  res.send("hello check");
-});
+app.get("/",(req,res)=>{
+    res.send("hello check")
+})
 
-const PORT = 5000;
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Failed to start server due to MongoDB connection error:', error);
-    process.exit(1);
+
+
+  app.listen(5000, () => {
+    console.log('🚀 Server running on http://localhost:5000');
   });
+
