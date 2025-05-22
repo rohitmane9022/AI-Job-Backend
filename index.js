fbbcf7ca091f07ef14efa802a5c930e0acffacd8
Rohit Mane <74919679+rohitmane9022@users.noreply.github.com>
2025-05-22 16:36:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 connectDB()
 
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 //Routes Check
